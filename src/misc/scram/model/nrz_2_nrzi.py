7197717e20ab3ec8234d508bb2277120d263f86e
julloa <joaquin.g.ulloa@gmail.com>
2023-10-14 19:57:15 -0300

    Se modifica el modelo nrzi para que trabaje con palabras de 10 bits

--- src/misc/scram/model/nrz_2_nrzi.py
+++ src/misc/scram/model/nrz_2_nrzi.py
@@ -1,8 +1,11 @@
-def nrz_2_nrzi(input_bits):
+def nrz_2_nrzi_bit(input_bits):
   """Converts an NRZ signal to an NRZI signal.
   Args: input_bits: A list of bits representing the NRZ signal.
   Returns: A list of bits representing the NRZI signal.
   """
+
+  nrzi_signal = []  # Initialize an empty list to store the NRZI signal
+  current_state = 0  # Initialize the current state to 0 (low)
 
   for bit in input_bits:
     if bit == 1:
@@ -11,8 +14,28 @@ def nrz_2_nrzi(input_bits):
 
   return nrzi_signal
 
+def nrz_2_nrzi(input_bits):
+
+  nrzi_signals = []
+
+  for input_bits in input_words:
+    nrzi_signal = nrz_2_nrzi_bit(input_bits)
+    nrzi_signals.append(nrzi_signal)
+
+  return nrzi_signals
+
 # Example usage:
 # input_bits = [0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0]
-nrz_signal = ['1011001011']
-input_bits = nrz_to_nrzi(input_bits)
-print(input_bits)
+# input_bits = nrz_to_nrzi(input_bits)
+# print(input_bits)
+
+input_words = [
+  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+  [0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0]
+]
+
+nrzi_signals = nrz_2_nrzi(input_words)
+for nrzi_signal in nrzi_signals:
+    print(nrzi_signal)
